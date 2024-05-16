2591386076be116d6c068937a604d7db66faf2b4
iifau <144337438+iifau@users.noreply.github.com>
2024-05-16 23:15:06 +0300

    Update gr_b.py: реализация простой игры с тестом
    Добавлена и протестирована первая версия метода play. Добавлен класс My_Queue, чтобы определить очереди функцию длины.

--- gr_b.py
+++ gr_b.py
@@ -1,3 +1,10 @@
+from datetime import *
+from queue import *
+
+class My_Queue(Queue):
+    def size(self):
+        return len(self.queue)
+
 class Card:
     def __init__(self, averse, reverse, stage, nxt_rep):
         self.averse = averse
@@ -15,12 +22,31 @@ class Card:
         self.averse = new_awerse
     def edit_reverse(self, new_reverse):
         self.reverse = new_reverse
+
 class Deck:
-    def __init__(self, name, card_list):
+    def __init__(self, name, card_set):
         self.name = name
         self.card_set = card_set
     def add_card(self, new_card):
         self.card_set.add(new_card)
     def remove_card(self, target_card):
         self.card_set.remove(target_card)
-    #реализовать создание и удаление вспомогательных списков для проведения игры
+    def play(self):
+        game_deck = My_Queue() #узнать про prioritized queue и возможно её и сделать
+        for card in self.card_set:
+            if True: #заменить на условие необходимости повторить карточку
+                game_deck.put(card)
+        while game_deck.size() > 0:
+            curr_card = game_deck.get()
+            print(curr_card.averse)
+            if input().lower() == curr_card.reverse:
+                print('correct')
+            else:
+                print('incorrect, ' + curr_card.reverse)
+                game_deck.put(curr_card)
+        print('done')
+
+c1 = Card('дом', 'maison', 1, 1)
+c2 = Card('вокзал', 'gare', 1, 1)
+d = Deck('french words', (c1, c2))
+d.play()
