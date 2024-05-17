9dc7e3dd21835f4c5d0fdde88c4e328eab534a34
iifau <144337438+iifau@users.noreply.github.com>
2024-05-17 03:17:53 +0300

    Update gr_b.py: начальная реализация интервального повторения
    Добавлена система интервального повторения (интервалы, стадии, подстадии пока наобум) и из неё вытекающее. Реализованы методы игры (влияет на данные о следующем повторении карточки) и дополнительного повторения (никак не меняет параметры карточки, просто игра). Добавлен метод обновления информации о стадии, подстадии и времени следующего повторения после игры. На данный момент не обновляется время следующего повторения, постараюсь к следующей версии починить.

--- gr_b.py
+++ gr_b.py
@@ -6,22 +6,47 @@ class My_Queue(Queue):
         return len(self.queue)
 
 class Card:
-    def __init__(self, averse, reverse, stage, nxt_rep):
+    def __init__(self, averse, reverse, stage, substage, nxt_rep):
         self.averse = averse
         self.reverse = reverse
         self.stage = stage
+        self.substage = substage
         self.nxt_rep = nxt_rep
     def update_nxt_rep(self):
-        pass
+        if self.stage == 1 and self.substage == 1:
+            self.nxt_rep == datetime.now()
+        elif self.stage == 1:
+            self.nxt_rep == datetime.now() + timedelta(minutes=20)
+        elif self.stage == 2:
+            self.nxt_rep == datetime.now() + timedelta(hours=8)
+        elif self.stage == 3:
+            self.nxt_rep == datetime.now() + timedelta(days=1)
+        elif self.stage == 4:
+            self.nxt_rep == datetime.now() + timedelta(days=3)
+        else:
+            self.nxt_rep == datetime.now() + timedelta(weeks=1)
     def update_stage(self, progr):
         if progr and self.stage < 5:
             self.stage += 1
+            self.substage = 1
         elif not progr and self.stage > 1:
-            self.stage -= 1    
+            self.stage -= 1
+            self.substage = 3
+    def update_substage(self, progr):
+        if progr and self.substage < 3:
+            self.substage += 1
+        elif not progr and self.substage > 1:
+            self.substage -= 1
+        else:
+            self.update_stage(progr)
     def edit_averse(self, new_averse):
         self.averse = new_awerse
     def edit_reverse(self, new_reverse):
         self.reverse = new_reverse
+    def edit_state(self, new_stage, new_substage, new_nxt_rep): #это запрещённая техника, нужно ли прятать? или нужно или сливать с update-ами?
+        self.stage = new_stage
+        self.substage = new_substage
+        self.nxt_rep = new_nxt_rep
 
 class Deck:
     def __init__(self, name, card_set):
@@ -31,22 +56,55 @@ class Deck:
         self.card_set.add(new_card)
     def remove_card(self, target_card):
         self.card_set.remove(target_card)
+    def extra_rep(self):
+        for curr_card in self.card_set:
+            while True: #может это тоже неоч?
+                print(curr_card.averse)
+                if input().lower() == curr_card.reverse.lower():
+                    print('correct')
+                    break
+                else:
+                    print('incorrect, ' + curr_card.reverse)          
     def play(self):
         game_deck = My_Queue() #узнать про prioritized queue и возможно её и сделать
+        played = {}
         for card in self.card_set:
-            if True: #заменить на условие необходимости повторить карточку
+            if card.nxt_rep <= datetime.now():
                 game_deck.put(card)
         while game_deck.size() > 0:
             curr_card = game_deck.get()
-            print(curr_card.averse)
-            if input().lower() == curr_card.reverse:
-                print('correct')
+            if curr_card.nxt_rep > datetime.now():
+                played[(averse, reverse)] = (curr_card.stage, curr_card.substage, curr_card.nxt_rep) #это вообще нормально?
             else:
-                print('incorrect, ' + curr_card.reverse)
-                game_deck.put(curr_card)
-        print('done')
+                print(curr_card.averse)
+                if input().lower() == curr_card.reverse.lower():
+                    print('correct')
+                    curr_card.update_substage(True)
+                    curr_card.update_nxt_rep()
+                    if curr_card.stage == 1 and curr_card.substage == 1:
+                        game_deck.put(curr_card)
+                    else:
+                        played[(curr_card.averse, curr_card.reverse)] = (curr_card.stage, curr_card.substage, curr_card.nxt_rep) #видимо тут проблема с nxt_rep, когда карточка свежая
+                else:
+                    print('incorrect, ' + curr_card.reverse)
+                    curr_card.update_substage(False)
+                    curr_card.nxt_rep = datetime.now()
+                    game_deck.put(curr_card)
+        for card in self.card_set:
+            print('updating data') #отладка
+            card_data = (card.averse, card.reverse)
+            new_stage, new_substage, new_nxt_rep = played[card_data]
+            print(new_stage, new_substage, new_nxt_rep, 'new data') #отладка
+            card.edit_state(new_stage, new_substage, new_nxt_rep)
+        print('no more cards, wanna study some more?')
+        answer = input()
+        if answer == 'yes':
+            self.extra_rep()
 
-c1 = Card('дом', 'maison', 1, 1)
-c2 = Card('вокзал', 'gare', 1, 1)
+c1 = Card('дом', 'maison', 1, 1, datetime.now())
+c2 = Card('вокзал', 'gare', 1, 1, datetime.now())
 d = Deck('french words', (c1, c2))
-d.play()
+while True:
+    d.play()
+    for card in d.card_set:
+        print(card.averse, card.reverse, card.stage, card.substage, card.nxt_rep)
